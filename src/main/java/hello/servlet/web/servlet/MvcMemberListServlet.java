0ddf56dad25157068ee4ba989e7cf10b03da2f13
alpin87 <qortmdals94@naver.com>
2024-03-27 19:48:15 +0900

    jsp:user managerment

---
+++ src/main/java/hello/servlet/web/servlet/MvcMemberListServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.servlet;
+
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepositoty;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.tags.shaded.org.apache.regexp.RE;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name ="MvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+private MemberRepositoty memberRepositoty = MemberRepositoty.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepositoty.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/save-result.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qortm
+  Date: 2024-03-27
+  Time: 오후 7:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+성공
+<ui>
+    <li>id = ${member.id}</li>
+    <li>user =${member.username}</li>
+    <li>age =${member.age}</li>
+</ui>
+<a href="/index.html">메인</a>
+</body>
+</html>
